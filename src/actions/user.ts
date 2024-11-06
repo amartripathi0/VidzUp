82c1aa671defc61cfa5afbe196a126c060941490
amartripathi0 <theamartripathi@gmail.com>
2024-11-06 22:23:48 +0530

    add searchUser action

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -61,7 +61,7 @@ export const onAuthenticateUser = async () => {
 
     if (newUser) return { status: 201, newUser };
   } catch (error) {
-      return { status: 500 };
+    return { status: 500 };
   }
 };
 
@@ -73,26 +73,62 @@ export const getNotifications = async () => {
       return { status: 403 };
     }
 
-    const notifications  = await prisma.user.findUnique({
-      where : {
-        clerkid : user.id
+    const notifications = await prisma.user.findUnique({
+      where: {
+        clerkid: user.id,
       },
-      select : {
-        notification : true,
-        _count : {
-          select : {
-            notification:true
-          }
-        }
-      }
-    })
+      select: {
+        notification: true,
+        _count: {
+          select: {
+            notification: true,
+          },
+        },
+      },
+    });
 
-    if(notifications && notifications.notification.length >0) {
-      return {status : 200 , notifications}
+    if (notifications && notifications.notification.length > 0) {
+      return { status: 200, notifications };
     }
-    return { status: 403, notifications : [] };
+    return { status: 403, notifications: [] };
   } catch (error) {
-        return { status: 403, notifications: [] };
+    return { status: 403, notifications: [] };
+  }
+};
 
+export const searchUsers = async (query: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+
+    const users = await prisma.user.findMany({
+      where: {
+        OR: [
+          { firstname: { contains: query } },
+          { email: { contains: query } },
+          { lastname: { contains: query } },
+        ],
+        NOT: [{ clerkid: user.id }],
+      },
+      select: {
+        id: true,
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+        firstname: true,
+        lastname: true,
+        image: true,
+        email: true,
+      },
+    });
+    if (users && users.length > 0) {
+      return { status: 200, data: users };
+    }
+
+    return { status: 404, data: undefined };
+  } catch (error) {
+    return { status: 500, data: undefined };
   }
 };
